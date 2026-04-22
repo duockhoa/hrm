1d073bbeb19ff88b356f46c544225a46597933b5
Binh <dbcl@dkpharma.vn>
2026-04-22 22:14:48 +0700

    thêm page

---
+++ frontend/src/app/(mainlayout)/company/[id]/page.tsx
@@ -0,0 +1,8 @@
+export default function CompanyDetailPage() {
+  return (
+    <div className="bg-white rounded-md p-4 shadow-md h-[100%]">
+      <h1 className="text-2xl font-bold mb-4">Company Detail</h1>
+      <p>Company details will be displayed here.</p>
+    </div>
+  );
+}
